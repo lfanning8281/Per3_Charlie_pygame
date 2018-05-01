d86e5c084c804c6db2f7c9c0d76d8e8eb32d4dd2
Leah Fanning <LFanning8281@student.pps.net>
2018-05-01 11:07:23 -0700

    Change screen dimensions

--- game_window.py
+++ game_window.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 import pygame
-screen = pygame.display.set_mode((640, 480))
+screen = pygame.display.set_mode((500, 500))
 running=True
 while running:
 	event=pygame.event.poll()
