fe81fa6d79fe863c86ea6ec945c1758dd3e0a1f9
Leah Fanning <LFanning8281@student.pps.net>
2018-05-15 10:56:39 -0700

    Change color to correlate with inputted time

--- game_window.py
+++ game_window.py
@@ -2,12 +2,20 @@
 import pygame
 width=int(input("Input width "))
 height=int(input("Input height "))
+color=[0,0,0]
+time=int(input("What is the time? (Use military time) "))
+if time<1200:
+	color[2]=(time/10)
+if time==1200:
+	color[2]=255
+if time>1200:
+	color[2]=255-(time/10)
 screen = pygame.display.set_mode((width, height))
 running=True
 while running:
 	event=pygame.event.poll()
 	if event.type==pygame.QUIT:
 		running=False
-	screen.fill((255,255,255))
+	screen.fill((color))
 	pygame.display.flip()
 	
