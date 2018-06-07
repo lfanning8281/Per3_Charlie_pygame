5288468d6bd9ca4cdd1a52d9a0fbfdf1b83bf797
Leah Fanning <LFanning8281@student.pps.net>
2018-06-07 12:30:42 -0700

    Add import for snake_game

--- Snake_pygame.py
+++ Snake_pygame.py
@@ -1,5 +1,6 @@
 import pygame, random, sys
 from pygame.locals import *
+import snake_game
 def collide(x1, x2, y1, y2, w1, w2, h1, h2):
 	if x1+w1>x2 and x1<x2+w2 and y1+h1>y2 and y1<y2+h2:return True
 	else:return False
